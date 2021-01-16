3d4f978f7be99637da900582e526f531d0acac23
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-16 22:09:43 +0800

    Add a signup page

--- src/layouts/Body.js
+++ src/layouts/Body.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Home, Login, Dashboard } from "../views"
+import { Home, Login, Dashboard, SignUp } from "../views"
 import { useState } from 'react';
 import GuardedRoute from 'react-guarded-route';
 
@@ -15,6 +15,7 @@ export default function Body() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" render={props => <Login authenticate={authenticate} />} />
+        <Route path="/signup" component={SignUp} />
         <GuardedRoute
           path="/dashboard"
           component={Dashboard}
--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import SignUp from "./SignUp";
 
-export { Home, Login, Dashboard }; 
+export { Home, Login, Dashboard, SignUp }; 
