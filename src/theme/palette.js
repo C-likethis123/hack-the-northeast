c3fcfc593daeabaa5e11cb79ef5ed9d333b13f39
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-16 19:12:47 +0800

    Create palette and typography

---
+++ src/theme/palette.js
@@ -0,0 +1,12 @@
+const palette = {
+  palette: {
+    primary: {
+      main: "#FFBD66",
+    },
+    secondary: {
+      main: "#fff",
+    },
+  },
+};
+
+export default palette;
---
+++ src/theme/typography.js
@@ -0,0 +1,13 @@
+const typography = {
+  typography: {
+    h1: {
+      fontSize: '3rem',
+      fontWeight: 'bold',
+    },
+    body2: {
+      fontSize: '1.125rem',
+    },
+  },
+};
+
+export default typography;
