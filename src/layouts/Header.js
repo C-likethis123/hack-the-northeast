877ab682aa3286d6f4ca5a6813ae1e6eadc34e17
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-17 01:29:00 +0800

    Change css of header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core";
 
 export default function Header() {
+  const classes = useStyles()
   return (
-    <div style = {{backgroundColor: "#FFBD66"}}>
-      <header >INTERNSEARCH</header>
-    </div>
+    <header className={classes.header}>JOB BOX</header>
   )
 }
 
+const useStyles = makeStyles((theme) => ({
+  header: {
+    backgroundColor: theme.palette.primary.main,
+    color: "white",
+    fontSize: '1.125rem',
+    paddingLeft: '1.25rem',
+    paddingTop: '1.25rem',
+    paddingBottom: '1.25rem',
+  },
+}));
