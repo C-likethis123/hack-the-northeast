1c7b4ed9ec4dde1f44d0f5814270b5882379f26b
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-16 22:10:41 +0800

    Demonstrate JSS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
-import { Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import { Body, Footer, Header } from './layouts';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from "./theme";
 
 function App() {
+  const classes = useStyles();
   return (
-    <Container>
+    <ThemeProvider theme={theme}>
       <Header />
       <Body />
       <Footer />
-    </Container>
+      {/* <div className={classes.pinkBackground}>
+        <div class="red">Red1</div>
+        <div class="red">Red1</div>
+
+        <div class="red">Red1</div>
+        <div class="red">Red1</div>
+        <div class="red">Red1</div>
+
+      </div> */}
+    </ThemeProvider>
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+  background: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  pinkBackground: {
+    backgroundColor: 'pink',
+    // adding your own css selectors to target more nested classes
+    '& .red': {
+      backgroundColor: 'red',
+    }
+  }
+}));
 export default App;
