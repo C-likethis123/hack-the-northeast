0bbd272f6ca96663987673dc5db621adb9f9cd23
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-17 14:33:43 +0800

    Add api for students

--- server/index.js
+++ server/index.js
@@ -27,6 +27,49 @@ app.post('/login', (req, res) => {
     })
   }
 })
+const Students = [
+  {
+    userId: 1,
+    userType: "student",
+    skills: ['React', 'Javascript'],
+    experiences: [{
+      company: 'Facebook',
+      jobTitle: 'Software Engineering Intern',
+      employmentType: 'Intern',
+      from: '18-May-2020',
+      to: '18-Aug-2020',
+      description: 'I did a lot in Facebook.'
+    },
+    {
+      company: 'NUS IT',
+      jobTitle: 'Software Engineering Intern',
+      employmentType: 'Intern',
+      from: '18-May-2019',
+      to: '18-Aug-2019',
+      description: 'I did a lot in Facebook.'
+    }],
+    preferences: {
+      jobType: ['Frontend Development', 'UI/UX'],
+      companyType: ['Startups', 'Government Agencies'],
+      workArrangement: ['Remote'],
+    }
+  }
+]
+// REST API for students
+app.get('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const student = Students.find(student => student.userId === id);
+  if (student) {
+    return res.status(200).json(student);
+  } else {
+    return res.status(404).json("No students found!");
+  }
+})
+
+app.post('/users', (req, res) => {
+  Students.push(req.body);
+  return res.status(200).json("User added!");
+})
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
